b546f6a165a67b1d0bf7c8dbac54e41faf9c3fa0
Zahi Akiva <zakiva@redhat.com>
2015-08-25 13:19:58 +0300

    Persist Pods Volumes

--- spec/models/ems_refresh/parsers/kubernetes_spec.rb
+++ spec/models/ems_refresh/parsers/kubernetes_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require 'recursive-open-struct'
 
 describe ManageIQ::Providers::Kubernetes::ContainerManager::RefreshParser do
   let(:parser)  { described_class.new }
@@ -91,6 +92,64 @@ describe ManageIQ::Providers::Kubernetes::ContainerManager::RefreshParser do
 
         result_image.should == ex[:image]
         result_registry.should == ex[:registry]
+      end
+    end
+  end
+
+  describe "parse_volumes" do
+    example_volumes = [
+        {
+            :volume => RecursiveOpenStruct.new(
+                :name => "example-volume1",
+                :gitRepo => {:repository => "default-git-repository"}),
+            :name => "example-volume1",
+            :git_repository => "default-git-repository",
+            :empty_dir_medium_type => nil,
+            :gce_pd_name => nil,
+            :common_fs_type => nil
+        },
+        {
+            :volume => RecursiveOpenStruct.new(
+                :name => "example-volume2",
+                :emptyDir => {:medium => "default-medium"}),
+            :name => "example-volume2",
+            :git_repository => nil,
+            :empty_dir_medium_type => "default-medium",
+            :gce_pd_name => nil,
+            :common_fs_type        => nil
+        },
+        {
+            :volume => RecursiveOpenStruct.new(
+                :name => "example-volume3",
+                :gcePersistentDisk => {:pdName => "example-pd-name",
+                                       :fsType => "default-fs-type"}),
+            :name => "example-volume3",
+            :git_repository => nil,
+            :empty_dir_medium_type => nil,
+            :gce_pd_name => "example-pd-name",
+            :common_fs_type        => "default-fs-type"
+        },
+        {
+            :volume => RecursiveOpenStruct.new(
+                :name => "example-volume4",
+                :awsElasticBlockStore => {:fsType => "example-fs-type"}),
+            :name => "example-volume4",
+            :git_repository => nil,
+            :empty_dir_medium_type => nil,
+            :gce_pd_name => nil,
+            :common_fs_type        => "example-fs-type"
+        }
+    ]
+    
+    it "tests example volumes" do
+      parsed_volumes = parser.send(:parse_volumes, example_volumes.collect do |ex| ex[:volume] end)
+
+      example_volumes.zip(parsed_volumes).each do |example, parsed|
+        parsed[:name].should == example[:name]
+        parsed[:git_repository].should == example[:git_repository]
+        parsed[:empty_dir_medium_type].should == example[:empty_dir_medium_type]
+        parsed[:gce_pd_name].should == example[:gce_pd_name]
+        parsed[:common_fs_type].should == example[:common_fs_type]
       end
     end
   end
