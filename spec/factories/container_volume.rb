2ba92cde20e1346b8f2f4d365f8d8d5984895379
Zahi Akiva <zakiva@redhat.com>
2017-03-15 12:14:01 +0200

    Revert "Merge pull request #13612 from zakiva/fix_pod_volumes"
    This reverts commit 534e54bf63ef803bfb1ffade4761a2442ba2b66d.

--- spec/factories/container_volume.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :container_volume do
-    sequence(:name) { |n| "container_volume_#{seq_padded_for_sorting(n)}" }
-  end
-end
--- spec/factories/persistent_volume.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :persistent_volume do
-    sequence(:name) { |n| "persistent_volume_#{seq_padded_for_sorting(n)}" }
-  end
-end
